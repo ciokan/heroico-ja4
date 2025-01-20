56c26597220306f03da8a551d02f8d8dceffa555
Romeo Mihalcea <eu@ciokan.ro>
2025-01-20 23:14:17 +0200

    chore: updates

--- index.ts
+++ index.ts
@@ -4,13 +4,11 @@ export const config = {
 
 export default async function handler(req: Request) {
 	return new Response(
-		JSON.stringify({
-			message: 'Hello, world!',
-		}),
+		JSON.stringify(req.headers.toJSON()),
 		{
 			status : 200,
 			headers: {
-				'content-type': 'application/json',
+				'content-type': 'text/json',
 			},
 		}
 	);
